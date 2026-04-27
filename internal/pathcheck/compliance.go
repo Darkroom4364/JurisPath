cf69f5206fae677810a583d9bf8857d0f3ed9c4d
Darkroom4364 <ro.peneder@gmail.com>
2026-04-27 15:08:01 +0200

    Guard CheckHopsCompliant against empty hops slice

--- internal/pathcheck/compliance.go
+++ internal/pathcheck/compliance.go
@@ -5,6 +5,10 @@ import "github.com/jurispath/jurispath/pkg/model"
 // CheckHopsCompliant returns the offending hops that violate the given policy mode
 // and allowed ISD set. Returns nil if all hops are compliant.
 func CheckHopsCompliant(hops []model.ASHop, allowedISDs []uint16, mode string) []model.ASHop {
+	if len(hops) == 0 {
+		return nil
+	}
+
 	allowed := make(map[uint16]bool, len(allowedISDs))
 	for _, isd := range allowedISDs {
 		allowed[isd] = true
